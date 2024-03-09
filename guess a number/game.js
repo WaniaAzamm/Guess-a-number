f33241078a4704977745f54711598ae259592921
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-03-10 01:14:43 +0500

    Add files via upload

---
+++ guess a number/game.js
@@ -0,0 +1,12 @@
+var winningNumber = 12;
+var userGuess = 43;
+if (userGuess == winningNumber) {
+    console.log("Wow, your guess is Correct!");
+}
+else if (userGuess > winningNumber) {
+    console.log("Too High..!");
+}
+else {
+    console.log("Too Low..!");
+}
+;
---
+++ guess a number/game.ts
@@ -0,0 +1,9 @@
+let winningNumber :number = 12;
+let userGuess :number = 43;
+if(userGuess == winningNumber){
+    console.log("Wow, your guess is Correct!")
+}else if(userGuess > winningNumber){
+    console.log("Too High..!")
+}else{
+    console.log("Too Low..!")
+};
