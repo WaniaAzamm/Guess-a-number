fad3531e1bfcec7f019f12a6160c4ca22cacc14b
Wania Azam <159264679+WaniaAzamm@users.noreply.github.com>
2024-03-18 01:40:52 +0500

    game.ts

--- guess a number/game.ts
+++ guess a number/game.ts
@@ -1,8 +1,14 @@
-let winningNumber :number = 12;
-let userGuess :number = 43;
-if(userGuess == winningNumber){
+import inquirer from "inquirer";
+let winningNumber :number =12;
+let userGuess = await inquirer.prompt(
+{
+   name:"userguess",
+   type:"number",
+   message:"Guess a Number!"
+})
+if(userGuess.userguess == winningNumber){
     console.log("Wow, your guess is Correct!")
-}else if(userGuess > winningNumber){
+}else if(userGuess.userguess > winningNumber){
     console.log("Too High..!")
 }else{
     console.log("Too Low..!")
